cc9ffc2f8a0905bed3d6d8500106bcfd88d15ecd
Nayanajith Pilapitiya <pilapitiyanss@gmail.com>
2024-02-17 04:04:05 +0530

    Added menuconfig and move menu to config

---
+++ src/appconfig/menuconfig.tsx
@@ -0,0 +1,52 @@
+import { TMainMenuType, TSubMenuType } from '@/types/TNavBar';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+export const mainMenuItems: TMainMenuType[] = [
+    {
+      Key: 'Dashboard',
+      Title: 'Dashboard',
+      Url: '/dashboard',
+      Icon: <DashboardIcon/>,
+      SubMenu: [],
+      IsExpand: false,
+    },
+    {
+      Key: 'Orders',
+      Title: 'Orders',
+      Url: '/orders',
+      Icon: <ShoppingCartIcon />,
+      SubMenu: [],
+      IsExpand: false,
+    },
+    {
+      Key: 'Customers',
+      Title: 'Customers',
+      Url: '',
+      Icon: <PeopleIcon />,
+      SubMenu: [
+        {
+          Key: 'NewCustomers',
+          Title: 'New Customers',
+          Url: '/new-customers',
+          Icon: <BarChartIcon />,
+        },
+      ],
+      IsExpand: false,
+    },
+  ];
+
+  export const secondaryMenuItems: TMainMenuType[] = [
+    {
+      Key: 'Reports',
+      Title: 'Reports',
+      Url: '/reports',
+      Icon: <AssignmentIcon />,
+      SubMenu: [],
+      IsExpand: false,
+    }
+  ];
+
